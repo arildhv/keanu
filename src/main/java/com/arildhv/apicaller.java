9bfe1e0d01448aa638af22c46205068fe9020a8f
arildhv <valderalv@hotmail.com>
2023-05-08 17:01:04 +0200

    Removed gradle, KISS

---
+++ src/main/java/com/arildhv/apicaller.java
@@ -0,0 +1,25 @@
+package com.arildhv;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class apicaller {
+    String apiUrl = "https://api.themoviedb.org/3/person/";
+
+    public apicaller(String key, String id){
+        String url = apiUrl + id + 
+        URL urlObj = new URL(apiUrl + )
+    }
+
+    public String getBirthdate()
+    
+    
+
+        System.out.println(response);
+
+        return "hello";
+    }
+
+}
--- src/main/java/com/arildhv/main.java
+++ src/main/java/com/arildhv/main.java
@@ -1,5 +1,16 @@
+package com.arildhv;
+
+import org.json.JSONObject;
+
 public class main {
     public static void main(String[] args) {
-        
+        String key = "b8663d389735608463f9334969f3f89d";
+        String keanuId = "6384";
+
+        apicaller caller = new apicaller(key, keanuId);
+
+        String birthdate = caller.getBirthdate();
+
+        System.out.println(caller.getBirthdate());
     }
 }
